3ae3790040a848b1a0146b9d8c8b2813df11d769
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-01-31 21:40:49 +0000

    Control Points (should) move associated bones by a specified weight
    Testing needs to be done tomorrow.

--- Assets/Scripts/ControlPoint.cs
+++ Assets/Scripts/ControlPoint.cs
@@ -25,11 +25,22 @@ public class ControlPoint : MonoBehaviour
     /// Toggle for if the face is currently distorting
     /// </summary>
     private bool b_distorting;
+    [SerializeField]
+    private Transform[] hb_bones;
+    /// <summary>
+    /// Bone weights are how far you wish to move the bones via.
+    /// </summary>
+    [SerializeField]
+    private float[] boneWeights;
+    private Vector3[] v3_origins;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        v3_origins = new Vector3[hb_bones.Length];
+        for (int i = 0; i < hb_bones.Length; i++)
+            v3_origins[i] = hb_bones[i].position;
     }
 
     // Update is called once per frame
@@ -46,7 +57,8 @@ public class ControlPoint : MonoBehaviour
     public void SetPosition(Vector3 _v3_delta)
     {
         rb.MovePosition(Vector3.Lerp(rb.position, _v3_delta, 0.95f));
-        
+        for (int i = 0; i < hb_bones.Length; i++)
+            hb_bones[i].position = v3_origins[i] + (transform.position - v3_origins[i]) * boneWeights[i];
     }
 
     /// <summary>
