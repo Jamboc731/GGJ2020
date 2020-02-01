a657224b59abb7517a6c7a15286883c66189486d
johnarthurmeakin <43747033+johnarthurmeakin@users.noreply.github.com>
2020-02-01 13:34:25 +0000

    Voice controls

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -2,6 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using System;
+
+
+public enum Character
+{
+    john,
+    bart,
+    brett,
+    liouse,
+    nat,
+    byron,
+    jame
+}
+
+
+[Serializable]
+public struct CharacterInfo
+{
+    public int i_voiceClip;
+    public float f_minPitch;
+    public float f_maxPitch;
+    public float f_minDelay;
+    public float f_maxDelay;
+    public Sprite face;
+}
+
+
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,7 +41,9 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource as_Music;
 
     private bool b_CanRepeatVoice;
-    private bool b_VoiceCoroutine;
+    private bool b_VoiceCoroutine = true;
+
+
 
     public void ToggleSFX()
     {
@@ -31,33 +60,43 @@ public class AudioManager : MonoBehaviour
 
     public void PlaySFX(int _ac_audioClipID)
     {
-        as_SFX.pitch = Random.Range(0.7f, 1.3f);
+        as_SFX.pitch = UnityEngine.Random.Range(0.7f, 1.3f);
         as_SFX.clip = acA_SFX[_ac_audioClipID];
-        as_SFX.PlayDelayed(Random.Range(0.8f, 1.5f));
+        as_SFX.PlayDelayed(UnityEngine.Random.Range(0.8f, 1.5f));
     }
 
-    public void PlayVoice(int _ac_audioClipID, float _f_RepeatTime)
+    public void PlayVoice(int _ac_audioClipID, float _f_RepeatTime, int _i_voicesToPlay, CharacterInfo _cv_chosenVoice)
     {
+
+        for (int i = 0; i < _i_voicesToPlay; i++)
+        {
+            StartCoroutine(PlayVoiceCoroutine(i - UnityEngine.Random.Range(_cv_chosenVoice.f_maxDelay, _cv_chosenVoice.f_minDelay) * i, _cv_chosenVoice);
+        }
+
+        /*
         if (b_VoiceCoroutine)
         {
             StartCoroutine(PlayVoiceCoroutine(_ac_audioClipID, _f_RepeatTime));
         }
         if (b_CanRepeatVoice)
         {
+        as_Voice.clip = acA_Voice[_ac_audioClipID];
             as_Voice.pitch = Random.Range(0.6f, 1.4f);
-            as_Voice.clip = acA_Voice[_ac_audioClipID];
-            as_Voice.PlayDelayed(Random.Range(0.8f, 1.5f));
             PlayVoice(_ac_audioClipID, _f_RepeatTime);
         }
+        */
     }
-    IEnumerator PlayVoiceCoroutine(int _ac_audioClipID, float _f_RepeatTime)
+    IEnumerator PlayVoiceCoroutine(float _f_RepeatTime, CharacterInfo _cv_chosenVoice)
     {
+        yield return new WaitForSeconds(_f_RepeatTime);
+        as_Voice.pitch = UnityEngine.Random.Range(_cv_chosenVoice.f_minPitch, _cv_chosenVoice.f_maxPitch);
+        as_Voice.PlayOneShot(acA_Voice[_cv_chosenVoice.i_voiceClip]);
+        /*
         b_CanRepeatVoice = true;
         b_VoiceCoroutine = false;
         PlayVoice(_ac_audioClipID, _f_RepeatTime);
-        yield return new WaitForSeconds(_f_RepeatTime);
         b_CanRepeatVoice = false;
-        b_VoiceCoroutine = true;
+        */
     }
 
 }
--- Assets/Audio/PlayAudio.cs
+++ Assets/Audio/PlayAudio.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayAudio : MonoBehaviour
 {
+    private AudioManager AM;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AM.PlayVoice(0, 5, 15);
+            print("pressed space");
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        AM = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
     }
 }
