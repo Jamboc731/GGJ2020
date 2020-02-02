1116d0b41517d149d24325fb2f7978567ff2be08
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-02-02 10:24:01 +0000

    LevelTests and Stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,13 +13,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private ControlPoint[] controlPoints;
 
     // An array that stores the story number that you're on, and the level.
-    [SerializeField] StoryToTargets storyToTargets;
-    private GameObject background;
+    [SerializeField] StoryToTargets stt_storyToTargets;
+    [SerializeField] private GameObject go_background;
     [TextArea] private string[] s_storyTexts;
     #endregion
     [SerializeField] [TextArea] private string s_textToShow;
     [SerializeField] [TextArea] private string s_errorText;
-    StoryToTargets[] targets;
+    StoryToTargets[] sttA_targets;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +34,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public bool LoadLevel(int _i_levelID)
+    public void LoadLevel(int _i_levelID)
     {
         // Load in parameters for the level
         LevelSO levelToLoad = null;
@@ -43,21 +44,19 @@ public class GameManager : MonoBehaviour
         if (levelToLoad != null)
         {
             // Set the level background and all the bones target points
-            background = levelToLoad.backgroundObject;
+            go_background = levelToLoad.backgroundObject;
             levelCharacter = levelToLoad.character;
             s_storyTexts = levelToLoad.texts;
-            targets = levelToLoad.storyTargets;
+            sttA_targets = levelToLoad.storyTargets;
             //for (int i = 0; i < levelToLoad.boneOrigins.Length; i++)
             //{
             //    storyToTargets.i_storyID[i] = i;
             //    for (int j = 0; j < storyToTargets.t_targetPoints.Length; j++)
             //        storyToTargets.t_targetPoints[j] = levelToLoad.boneOrigins[i][j];
             //}
-            return true;
         }
         else
             s_textToShow = s_errorText;
-            return false;
 
     }
 
@@ -66,7 +65,7 @@ public class GameManager : MonoBehaviour
         int storyID = Random.Range(0, s_storyTexts.Length);
         s_textToShow = s_storyTexts[storyID];
 
-        for (int i = 0; i < targets[storyID].t_targetPoints.Length; i++)
-            controlPoints[i].v3_TargetPoint = targets[storyID].t_targetPoints[i];
+        for (int i = 0; i < sttA_targets[storyID].t_targetPoints.Length; i++)
+            controlPoints[i].v3_TargetPoint = sttA_targets[storyID].t_targetPoints[i];
     }
 }
